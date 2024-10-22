7b51a4fe2d4253dd94bb9564d163974221779d31
IvoCalveira <calveira.ivo@gmail.com>
2024-10-22 15:48:16 -0300

    esta todo mas lindo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var jwt = require('jsonwebtoken');
 
 var app = express();
 // app.use(express.json());
-
+app.use(cors()); 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors()); 
@@ -26,7 +26,7 @@ app.get('/prueba/', (req, res) => {
 
 });
 
-app.post('/login/', (req,res)  => {
+app.post('/login', (req,res)  => {
 
     var usuario = req.body;
 
